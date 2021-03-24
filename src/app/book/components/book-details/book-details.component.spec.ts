8cba8da1a9aaa123dd5e3d4faa45362d34faec36
Marek Matczak <mmatczak@autograf.pl>
2021-03-24 16:03:05 +0100

    tests

--- src/app/book/components/book-details/book-details.component.spec.ts
+++ src/app/book/components/book-details/book-details.component.spec.ts
@@ -1,25 +1,73 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { BookDetailsComponent } from './book-details.component';
+import {BookDetailsComponent} from './book-details.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {Book} from '../../model/book';
 
 describe('BookDetailsComponent', () => {
-  let component: BookDetailsComponent;
-  let fixture: ComponentFixture<BookDetailsComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BookDetailsComponent ]
-    })
-    .compileComponents();
+  describe('(class tests)', () => {
+    it('fires bookChange event on save', () => {
+      // given
+      const eventMock: any = {
+        preventDefault: jasmine.createSpy('preventDefault'),
+        target: {
+          querySelector(selector: string): any {
+            return {
+              value: selector === 'input#author' ? 'Test Author' : 'Test Title'
+            };
+          }
+        }
+      };
+      const component = new BookDetailsComponent();
+      component.bookChange.subscribe(updatedBook => {
+        // then
+        expect(eventMock.preventDefault).toHaveBeenCalled();
+        expect(updatedBook.author).toBe('Test Author');
+        expect(updatedBook.title).toBe('Test Title');
+      });
+      // when
+      component.save(eventMock);
+    });
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BookDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  describe('(DOM tests)', () => {
+    let fixture: ComponentFixture<BookDetailsComponent>;
+    let component: BookDetailsComponent;
+    let element: HTMLElement;
+    let testBook: Book;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [BookDetailsComponent]
+      });
+      fixture = TestBed.createComponent<BookDetailsComponent>(BookDetailsComponent);
+      component = fixture.componentInstance;
+      element = fixture.nativeElement as HTMLElement;
+      testBook = {id: 0, title: 'Test Title', author: 'Test Author'};
+    });
+
+    it('renders author and title of input book', () => {
+      // given
+      component.book = testBook;
+      // when
+      fixture.detectChanges();
+      // then
+      const authorInput = element.querySelector<HTMLInputElement>('input#author');
+      expect(authorInput?.value).toBe(testBook.author);
+      const titleInput = element.querySelector<HTMLInputElement>('input#title');
+      expect(titleInput?.value).toBe(testBook.title);
+    });
+
+    it('fires bookChange event on button click', () => {
+      // given
+      component.book = testBook;
+      fixture.detectChanges();
+      const button = element.querySelector<HTMLButtonElement>('button');
+      component.bookChange.subscribe(updatedBook => {
+        // then
+        expect(updatedBook.author).toBe(testBook.author);
+        expect(updatedBook.title).toBe(testBook.title);
+      });
+      // when
+      button?.click();
+    });
   });
 });
